5a40ed3c9af43f3baee98d109ee8a4b367a23151
Huylb <bahuy9903@gmail.com>
2026-03-26 16:56:29 +0700

    results train

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+import os
+os.environ["TQDM_DISABLE"] = "1"
+
 from ultralytics import YOLO
 
-# model = YOLO('yolov8n.pt')
-model = YOLO("D:/AI_CDT/helipad-marker-detection/models/yolov8n.yaml")
+model = YOLO("D:/AI_CDT/helipad-marker-detection/models/yolov8n.pt")
 
 results = model.train(
     data="D:/AI_CDT/helipad-marker-detection/data.yaml",
     epochs=50,
     imgsz=320,
-    batch=8,       # hoặc 16 nếu GPU đủ VRAM
+    batch=8,
     device='cuda',
-    workers=0      # 🔑 tắt multiprocessing
+    workers=0,
+    verbose=False
 )
